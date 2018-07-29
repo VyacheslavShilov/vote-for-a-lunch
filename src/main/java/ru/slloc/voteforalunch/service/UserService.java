3db4bccbcc57e38b11ab72b3a6588e787998514f
Vyacheslav Shilov <rredlis@gmail.com>
2018-07-29 18:28:30 +0500

    Сделал ЮзерСервис + Кэш + Утильные классы

---
+++ src/main/java/ru/slloc/voteforalunch/service/UserService.java
@@ -0,0 +1,26 @@
+package ru.slloc.voteforalunch.service;
+
+
+
+
+import ru.slloc.voteforalunch.model.User;
+import ru.slloc.voteforalunch.util.exception.NotFoundException;
+
+import java.util.List;
+
+public interface UserService {
+
+    User create(User user);
+
+    void delete(int id) throws NotFoundException;
+
+    User get(int id) throws NotFoundException;
+
+    User getByEmail(String email) throws NotFoundException;
+
+    void update(User user);
+
+    List<User> getAll();
+
+   // User getWithMeals(int id);
+}
---
+++ src/main/java/ru/slloc/voteforalunch/util/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package ru.slloc.voteforalunch.util.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
