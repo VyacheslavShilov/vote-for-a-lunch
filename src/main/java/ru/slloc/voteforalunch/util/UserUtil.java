a32922087b423b0e2d57d411f47591dff869f954
Vyacheslav Shilov <rredlis@gmail.com>
2018-10-04 21:21:13 +0500

    fixes;

--- src/main/java/ru/slloc/voteforalunch/util/UserUtil.java
+++ src/main/java/ru/slloc/voteforalunch/util/UserUtil.java
@@ -28,7 +28,7 @@ public class UserUtil {
     public static User prepareToSave(User user, PasswordEncoder passwordEncoder) {
         String password = user.getPassword();
         user.setPassword(StringUtils.isEmpty(password) ? password : passwordEncoder.encode(password));
-        user.setEmail(user.getEmail().toLowerCase());
+        user.setEmail(user.getEmail().trim().toLowerCase());
         return user;
     }
 }
--- src/main/java/ru/slloc/voteforalunch/util/exception/NotFoundException.java
+++ src/main/java/ru/slloc/voteforalunch/util/exception/NotFoundException.java
@@ -1,8 +1,10 @@
 package ru.slloc.voteforalunch.util.exception;
 
 
+import org.springframework.lang.NonNull;
+
 public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
+    public NotFoundException(@NonNull String message) {
         super(message);
     }
 }
